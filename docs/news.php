49e6a41c4817621500affe9e37e644d5fad3d009
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-12-12 21:23:32 +0800

    Organized the codes and their alignment

--- docs/news.php
+++ docs/news.php
@@ -93,33 +93,58 @@ include '../backend/db.php';;
     <!-- News Section -->
     <div class="news-section">
 
-<?php
-$sql = "SELECT * FROM News";
-$result = $conn->query($sql);
+    <?php
+    $sql = "SELECT * FROM News";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo '
-        <div class="news-card">
-            <a href="' . $row["SOURCE_URL"] . '" target="_blank">
-                <img src="' . $row["HEADLINE_IMAGE_PATH"] . '" alt="news_image">
-            </a>
-            <h6>' . $row["HEADLINE"] . '</h6>
-            <div class="news-company">
-                <p><b>' . $row["ORGANIZATION"] . '</b></p>
-                <div class="ellipse"></div>
-                <p>' . $row["DATE_POSTED"] . '</p>
-            </div>
-            <p>' . $row["SUMMARY"] . '</p>
-            <div class="author-category-section">
-                <p>By: <a href="' . $row["SOURCE_URL"] . '" target="_blank">' . $row["AUTHOR"] . '</a></p>
-            </div>
-        </div>';
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo '
+            <div class="news-card">
+                <a href="' . $row["SOURCE_URL"] . '" target="_blank">
+                    <img src="' . $row["HEADLINE_IMAGE_PATH"] . '" alt="news_image">
+                </a>
+                <h6>' . $row["HEADLINE"] . '</h6>
+                <div class="news-company">
+                    <p><b>' . $row["ORGANIZATION"] . '</b></p>
+                    <div class="ellipse"></div>
+                    <p>' . $row["DATE_POSTED"] . '</p>
+                </div>
+                <p>' . $row["SUMMARY"] . '</p>
+                <div class="author-category-section">
+                    <p>By: <a href="' . $row["SOURCE_URL"] . '" target="_blank">' . $row["AUTHOR"] . '</a></p>
+                </div>
+            </div>';
+        }
+    } else {
+        echo "No news inserted yet.";
     }
-} else {
-    echo "No news inserted yet.";
-}
-?>
+    ?>
+
+    <div class="news-card">
+        
+      <a href = "https://newsinfo.inquirer.net/2145802/classes-suspended-shift-to-online-on-nov-25-due-to-inclement-weather" target="_blank">
+          <img src="images/suspension_photo.jpg" alt="suspension_photo">
+      </a>
+      <h6> Classes suspended, shifted to alternate mode in parts of PH on Nov. 25 </h6>
+      <div class="news-company">
+        <p><b>Inquirer Net</b></p>
+        <div class="ellipse"></div>
+        <p> 3 hours ago </p>
+      </div>
+
+      <p> MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
+          delivery mode (ADM) / alternative learning modalities on Tuesday, due to the effects of Tropical Depression
+          Verbena. </p>
+
+      <div class="author-category-section">
+        <p> By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a> </p>
+        <div class="category-container">
+          <p> Weather </p>
+        </div>
+      </div>
+
+    </div>
 
       <div class="news-card">
         
@@ -133,24 +158,19 @@ if ($result->num_rows > 0) {
           <p> 3 hours ago </p>
         </div>
 
-        <p>
-          MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
+        <p> MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
           delivery mode (ADM) / alternative learning modalities on Tuesday, due to the effects of Tropical Depression
-          Verbena.
-        </p>
+          Verbena.</p>
 
         <div class="author-category-section">
-          <p>
-            By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a>
-          </p>
+          <p> By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a> </p>
           <div class="category-container">
             <p> Weather </p>
           </div>
         </div>
-
       </div>
 
-            <div class="news-card">
+      <div class="news-card">
         
         <a href = "https://newsinfo.inquirer.net/2145802/classes-suspended-shift-to-online-on-nov-25-due-to-inclement-weather" target="_blank">
             <img src="images/suspension_photo.jpg" alt="suspension_photo">
@@ -162,45 +182,12 @@ if ($result->num_rows > 0) {
           <p> 3 hours ago </p>
         </div>
 
-        <p>
-          MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
+        <p> MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
           delivery mode (ADM) / alternative learning modalities on Tuesday, due to the effects of Tropical Depression
-          Verbena.
-        </p>
+          Verbena. </p>
 
         <div class="author-category-section">
-          <p>
-            By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a>
-          </p>
-          <div class="category-container">
-            <p> Weather </p>
-          </div>
-        </div>
-
-      </div>
-
-            <div class="news-card">
-        
-        <a href = "https://newsinfo.inquirer.net/2145802/classes-suspended-shift-to-online-on-nov-25-due-to-inclement-weather" target="_blank">
-            <img src="images/suspension_photo.jpg" alt="suspension_photo">
-        </a>
-        <h6> Classes suspended, shifted to alternate mode in parts of PH on Nov. 25 </h6>
-        <div class="news-company">
-          <p><b>Inquirer Net</b></p>
-          <div class="ellipse"></div>
-          <p> 3 hours ago </p>
-        </div>
-
-        <p>
-          MANILA, Philippines - Numerous local government units have either suspended classes or shifted to alternative
-          delivery mode (ADM) / alternative learning modalities on Tuesday, due to the effects of Tropical Depression
-          Verbena.
-        </p>
-
-        <div class="author-category-section">
-          <p>
-            By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a>
-          </p>
+          <p> By: <a href="https://newsinfo.inquirer.net/byline/keith-clores" target="_blank"> Keith Clores </a> </p>
           <div class="category-container">
             <p> Weather </p>
           </div>
