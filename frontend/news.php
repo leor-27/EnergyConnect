46022a3f928f3531777755e52a71970f478e79b4
Leo Angelo Larre Agpoon <leoangelolarreagpoon@gmail.com>
2025-12-13 20:24:10 +0800

    Made a CSS folder

--- frontend/news.php
+++ frontend/news.php
@@ -9,7 +9,7 @@ include '../backend/db.php';;
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title> Energy News </title>
-  <link rel="stylesheet" href="news.css">
+  <link rel="stylesheet" href="css/news.css">
   <script src="news-behavior.js"></script>
 </head>
 
